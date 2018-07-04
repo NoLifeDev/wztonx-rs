0860f0248e9142feee875b73abb6562bc963cb30
Peter Atashian <retep998@gmail.com>
2018-07-04 07:10:46 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+extern crate memmap;
+
+use std::env::args_os;
+use std::path::Path;
+
+fn main() {
+    let args: Vec<_> = args_os().skip(1).collect();
+    for arg in args {
+        let path = Path::new(&arg);
+        convert(path, &path.with_extension("nx"));
+    }
+}
+fn convert(inpath: &Path, outpath:&Path) {
+    println!("{:?} -> {:?}", inpath, outpath);
+}
